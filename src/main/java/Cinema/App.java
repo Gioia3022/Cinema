b50bfcedee67026ed98e0a8767837061164205a6
Gioia3022 <gioiagaliazzo04@gmail.com>
2022-03-20 00:06:36 +0100

    New JFrame

--- src/main/java/Cinema/App.java
+++ src/main/java/Cinema/App.java
@@ -1,7 +1,13 @@
 package Cinema;
 
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) {
+        JFrame j= new JFrame();
+        System.out.println("coucou");
+        //JXDatePicker d= new JXDatePicker();
+        //create calender;
         // write your code here
     }
 }
